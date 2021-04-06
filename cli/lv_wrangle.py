067542326f110ee94d6a961b828d2e9a34280c50
alexsquires <alex.g.squires@gmail.com>
2021-04-06 20:14:18 +0100

    fix the incar wrangling cli call

--- cli/lv_wrangle.py
+++ cli/lv_wrangle.py
@@ -5,19 +5,22 @@ from lazy_vasping.calc_wrangling import wrangle_incar
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directories', default='all', type=list,
+    parser.add_argument('-d', '--directories', default='all',
                         help='directories (containing INCARs) to edit')
-    parser.add_argument('-p', '--params', type=dict,
-                        help='dictionary of parameters to update')
+    parser.add_argument('-k', '--key', type=str,
+                        help='key of flag to update')
+    parser.add_argument('-v', '--value',
+                        help='value of flag to update')
     args = parser.parse_args()
 
     directories = args.directories
-    parameters_to_update = args.params
+    parameters_to_update = {args.key : args.value}
     
     if directories == 'all':
         directories = find_vasp_calculations()
+    
     for directory in directories:
-        wrange_incar(directory, paramters_to_updata)
+        wrangle_incar(directory, parameters_to_update)
 
 if __name__ == "__main__":
     main()
--- lazy_vasping/calc_wrangling.py
+++ lazy_vasping/calc_wrangling.py
@@ -9,7 +9,7 @@ def wrangle_incar(directory, calculation_parameters_to_update):
        calculation_parameters_to_update (dict): dictionary of calc paramters to be changed, where the key
        is the relevant VASP flag, and the values are the incar values.
    """
-    home=os.getcwd()
-    incar = Incar.from_file(f'{home}/{directory}/INCAR')
-    incar.update(calculation_parameters_to_update)
-    incar.write_file(f'{home}/{directory}/INCAR')
+   home=os.getcwd()
+   incar = Incar.from_file(f'{home}/{directory}/INCAR')
+   incar.update(calculation_parameters_to_update)
+   incar.write_file(f'{home}/{directory}/INCAR')
