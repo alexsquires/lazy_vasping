44ba7d738937d14429eacd2c8f2187a531f2259a
Thomas Young Cluster RITS Do NOT reset password <mmm0553@login10.michael.ucl.ac.uk>
2021-08-21 08:13:39 +0100

    add static run functionalilty

--- cli/lv_static.py
+++ cli/lv_static.py
@@ -16,20 +16,24 @@ def main():
 
     converged_calculations = []
     for converged_calculation in tqdm(to_run):
-        os.system(f'mkdir {converged_calculation}/../run.final')
-        outcar = Outcar(f'{converged_calculation}/OUTCAR')
-        mags = [i['tot'] for i in outcar.magnetization]
-        structure = Poscar.from_file(f'{converged_calculation}/CONTCAR').structure
-        structure.add_site_property('magmom',mags)
-        incar = Incar.from_file(f'{converged_calculation}/INCAR')
-        incar.update(static_params)
-        del incar['MAGMOM']
-        potcar = Potcar.from_file(f'{converged_calculation}/POTCAR')
-        calculation = DictSet(structure,{'INCAR':incar,'POTCAR':potcar})
-        calculation.incar.write_file(f'{converged_calculation}/../run.final/INCAR')
-        calculation.poscar.write_file(f'{converged_calculation}/../run.final/POSCAR')
-        os.system(f'cp {converged_calculation}/POTCAR {converged_calculation}/../run.final ; cp {converged_calculation}/job.sh {converged_calculation}/../run.final')
-        os.system(f'touch {converged_calculation}/../run.final/queued ; touch {converged_calculation}/../run.final/vasprun.xml')
+        if os.path.exists(f'mkdir {converged_calculation}/../run.final') == False:
+            os.system(f'mkdir {converged_calculation}/../run.final')
+            outcar = Outcar(f'{converged_calculation}/OUTCAR')
+            mags = [i['tot'] for i in outcar.magnetization]
+            structure = Poscar.from_file(f'{converged_calculation}/CONTCAR').structure
+            structure.add_site_property('magmom',mags)
+            incar = Incar.from_file(f'{converged_calculation}/INCAR')
+            incar.update(static_params)
+            try:
+                del incar['MAGMOM']
+            except:
+                None
+            potcar = Potcar.from_file(f'{converged_calculation}/POTCAR')
+            calculation = DictSet(structure,{'INCAR':incar,'POTCAR':potcar})
+            calculation.incar.write_file(f'{converged_calculation}/../run.final/INCAR')
+            calculation.poscar.write_file(f'{converged_calculation}/../run.final/POSCAR')
+            os.system(f'cp {converged_calculation}/POTCAR {converged_calculation}/../run.final ; cp {converged_calculation}/job.sh {converged_calculation}/../run.final')
+            os.system(f'touch {converged_calculation}/../run.final/vasp_out ; touch {converged_calculation}/../run.final/vasprun.xml')
 
 
 if __name__ == "__main__":
