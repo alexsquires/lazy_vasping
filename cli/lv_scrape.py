d63511475227f8b31599a71e7c6302e78d5ac6d4
zccaasq <zccaasq@login12.myriad.ucl.ac.uk>
2022-09-20 12:53:59 +0100

    add --force to scraping

--- cli/lv_scrape.py
+++ cli/lv_scrape.py
@@ -1,23 +1,32 @@
 from pymatgen.io.vasp import Vasprun, Outcar
 import json
 import pandas as pd
+import argparse
 from tqdm import tqdm
 
+
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--force')
+    args = parser.parse_args()
+
     df = pd.read_csv('calc_data.csv')
     converged = df['converged'] == True
     few_steps = df['ionic_steps'] < 10
 
-    to_scrape = list(df[converged & few_steps].iloc[:, 0])
+    if args.force:
+        to_scrape = df.iloc[:, 0]
+    else:
+        to_scrape = list(df[converged & few_steps].iloc[:, 0])
 
     converged_calculations = []
-    for converged_calculation in tqdm(to_scrape):
+    for converged_calculation in tqdm(to_scrape): 
         vr = Vasprun(f'{converged_calculation}/vasprun.xml', parse_potcar_file=False)
-            
-        entry = vr.get_computed_entry()
+         
+        entry = vr.get_computed_entry(data = ['incar'])
         entry.entry_id = converged_calculation
         entry_dict = entry.as_dict()
-       
         
         if vr.parameters['LORBIT'] == 11:
             outcar = Outcar(f'{converged_calculation}/OUTCAR')
--- cli/lv_submit.py
+++ cli/lv_submit.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from lazy_vasping.calc_submitting import restart_job, submit_job, find_new_vasp_calculations
 import os
-
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--force', action = "store_true")
+    args = parser.parse_args()
+
     df = pd.read_csv('calc_data.csv')
     converged = df['converged'] == True
     not_converged = df['converged'] == False
@@ -13,10 +17,15 @@ def main():
     not_running = df['last_updated'] >= 1800
 
     home=os.getcwd()
+    
+    if args.force == True:
+        to_restart = []
+        to_submit = list(df.iloc[:,0])
+    else:
+        to_restart = list(df[contcar & many_steps & not_running].iloc[:, 0]) + list(df[not_converged & contcar & not_running].iloc[:, 0])
+        to_submit = list(df[not_converged & not_contcar & not_running].iloc[:, 0]) + list(df[many_steps & not_running & not_contcar].iloc[:, 0])
+    
 
-    to_restart = list(df[contcar & many_steps & not_running].iloc[:, 0]) + list(df[not_converged & contcar & not_running].iloc[:, 0])
-    to_submit = list(df[not_converged & not_contcar & not_running].iloc[:, 0]) + list(df[many_steps & not_running & not_contcar].iloc[:, 0])
- 
     for directory in to_restart:
         restart_job(directory)
     for directory in to_submit:
